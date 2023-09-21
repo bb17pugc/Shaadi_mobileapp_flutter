fee6639b2ce0a20ce7c77f1919ed65dcb8f686f2
AliGosha <mianadnanaliali@gmail.com>
2023-09-21 11:20:22 +0500

    updated

---
+++ lib/components/UserInfo/UesrInfoMain.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UesrInfoMain extends StatefulWidget {
+
+  UesrInfoMain({super.key});
+  @override
+  State<UesrInfoMain> createState() => UesrInfoMainState();
+}
+
+class UesrInfoMainState extends State<UesrInfoMain> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("UesrInfoMain");
+  }
+}
